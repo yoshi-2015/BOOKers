3a04012f9703735eb81657d398401f2a14daa671
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2019-11-20 21:26:28 +0900

    [ADD]各ページ作成と遷移先設定・新規投稿機能・編集機能・デリートの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,13 +11,34 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    # if文でエラー吐いた時の対応を記述
+    flash[:notice] = "登録完了しました！Book was successfully created."
+    # rails routesで調べるとshowがbookとなっていた
+    # 更に登録したデータを保存するのはbook.saveのため、引数もbookでOK
+    redirect_to book_path(book)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    # if文でエラー吐いた時の対応を記述
+    flash[:notice] = "編集完了しました！Book was successfully updated."
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    # if文でエラー吐いた時の対応を記述
+    flash[:notice] = "削除完了しました！Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  # 入力された値が空の場合エラー扱いになる
+  validates :title, presence: true
+  validates :body, presence: true
 end
